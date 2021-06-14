8208d09142fabb317c18f33c9ae9b676d6ac9840
nyansawlinn1996 <51956676+nyansawlinn1996@users.noreply.github.com>
2021-06-14 23:18:08 +0630

    Revert "New Update"

--- js/modal.js
+++ js/modal.js
@@ -7,18 +7,18 @@ function displayModal(title){
     var modal_body = document.getElementById("modal_body");
 
     switch(title){
-        case 'Reporting Dashboard Design': modal_body.innerHTML ='<center><img src="img/Service Design.png" width=350px height=250px></center> <p> A data dashboard is an information management tool that visually tracks, analyzes and displays key performance indicators (KPI), metrics and key data points to monitor the health of a business, department or specific process. They are customizable to meet the specific needs of a department and company. </p>' ; break;
-        case 'Analytics': modal_body.innerHTML = '<p>Analytics is the systematic computational analysis of data or statistics. It is used for the discovery, interpretation, and communication of meaningful patterns in data. It also entails applying data patterns towards effective decision-making.</p> '; break;
+        case 'Reporting Dashboard Design': modal_body.innerHTML ='<strong><p>Strong P</p></strong><p>normal</p>'  ; break;
+        case 'Analytics': modal_body.innerText = 'Analytics '; break;
         case 'Analysis Architecture': modal_body.innerHTML = 'Analysis Architecture'; break;
-        case 'Data Governance': modal_body.innerHTML = '<p>Data Governance is the process, and procedure organizations use to manage, utilize, and protect their data. In this context, data can mean either all or a subset of a companys digital and/or hard copy assets. In fact, defining what data means to an organization is one of the data governance best practices </p>'; break;
+        case 'Data Governance': modal_body.innerHTML = 'Data Governance '; break;
         case 'Best Fit For Your Business': modal_body.innerHTML = 'Best Fit For Your Business'; break;
         case 'Change Management': modal_body.innerHTML = 'Change Management'; break;
     }
 
     modal.style.display = "block";
-
+  
   }
-
+  
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
@@ -32,3 +32,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
